5a1a148388461674702f38d60963a3708da1fa78
JMaue <j.mauersberger@freenet.de>
2024-10-17 22:01:12 +0200

    further development

--- Focus2Infinity/Data/DataHelper.cs
+++ Focus2Infinity/Data/DataHelper.cs
@@ -3,6 +3,8 @@
   public static class DataHelper
   {
     private static string Headline = "Headline";
+    private static string Details = "Details";
+
     public static string GetHeadline (this Dictionary<string, string> content)
     {
       if (content.ContainsKey(Headline))
@@ -16,10 +18,21 @@
     {
       foreach (var key in content.Keys)
       {
-        if (key == Headline)
+        if (key == Headline || key == Details)
           continue;
 
         yield return new KeyValuePair<string, string>(key, content[key]);
+      }
+    }
+
+    public static IEnumerable<KeyValuePair<string, string>> GetDetailedContent(this Dictionary<string, string> content)
+    {
+      foreach (var c in content.GetContent())
+        yield return c;
+
+      if (content.ContainsKey(Details))
+      {
+        yield return new KeyValuePair<string, string>(Details, content[Details]);
       }
     }
   }
